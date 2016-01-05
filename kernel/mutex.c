4d8e86d26d617ea2b72dd9a323ecc8bfe97fc1ea
genesixx <erwanleboucher@gmail.com>
2016-01-05 18:19:52 +0100

    Revert "mutex: Avoid label warning when !CONFIG_MUTEX_SPIN_ON_OWNER"
    This reverts commit acdfe49b9978c766349da7e7d1b12a1d60a30322.

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -489,8 +489,7 @@ __mutex_lock_common(struct mutex *lock, long state, unsigned int subclass,
 
 			mutex_set_owner(lock);
 			mspin_unlock(MLOCK(lock), &node);
-			preempt_enable();
-			return 0;
+			goto done;
 		}
 		mspin_unlock(MLOCK(lock), &node);
 
@@ -597,6 +596,7 @@ skip_wait:
 	}
 
 	spin_unlock_mutex(&lock->wait_lock, flags);
+done:
 	preempt_enable();
 	return 0;
 
